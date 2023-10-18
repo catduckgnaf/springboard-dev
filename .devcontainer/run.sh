19df6825b83845ecd23644cadb87cbc9ad9915ef
catduckgnaf <github@catduck.org>
2023-10-18 16:36:04 -0400

    kool kid notices
    edited initial run commands

---
+++ .devcontainer/run.sh
@@ -0,0 +1,1 @@
+echo 'installing pip requirements; it will not be long now' && pip install -qr .devcontainer/requirements.txt && uname -r && flask --version && cp .devcontainer/first-run-notice-temp.txt .devcontainer/first-run-notice.txt && cat .devcontainer/first-run-notice.txt && rm .devcontainer/first-run-notice.txt && bash
