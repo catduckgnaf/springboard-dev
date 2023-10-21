f7edb9544118797a69399314ee5e2eed9b6354b6
catduckgnaf <github@catduck.org>
2023-10-21 10:27:07 -0400

    cleaning up commands

--- .devcontainer/library-scripts/common-debian.sh
+++ .devcontainer/library-scripts/common-debian.sh
@@ -164,7 +164,11 @@ if [ "${PACKAGES_ALREADY_INSTALLED}" != "true" ]; then
 
     # Install pgadmin4 (and dependendencies to including postgresql) if not already installed (may be more recent than distro version)
     if ! type pgadmin4 > /dev/null 2>&1; then
-        apt-get -y install --no-install-recommends pgadmin4
+    curl -fsSL https://www.pgadmin.org/static/packages_pgadmin_org.pub | gpg --dearmor -o /etc/apt/trusted.gpg.d/pgadmin.gpg
+    sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list'
+    sudo apt update
+    apt-get -y install --no-install-recommends pgadmin4
+    
     fi
 
     PACKAGES_ALREADY_INSTALLED="true"
--- .devcontainer/library-scripts/quick-commands.sh
+++ .devcontainer/library-scripts/quick-commands.sh
@@ -13,3 +13,8 @@ sudo /etc/init.d/postgresql start
 # command to stop flask
 
 sudo /etc/init.d/postgresql stop
+
+
+# to set up pgadmin4
+
+sudo /usr/pgadmin4/bin/setup-web.sh
