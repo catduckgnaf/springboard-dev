2ffd376b53d91500061abdc944521951fd0accb1
catduckgnaf <github@catduck.org>
2023-12-14 16:27:21 -0500

    Update common-debian.sh

--- .devcontainer/library-scripts/common-debian.sh
+++ .devcontainer/library-scripts/common-debian.sh
@@ -179,14 +179,6 @@ if [ "${PACKAGES_ALREADY_INSTALLED}" != "true" ]; then
     apt-get -y install --no-install-recommends postgresql-plpython3-14
     fi
 
-    # Install pgadmin4 if not already installed (may be more recent than distro version)
-    if ! type pgadmin4 > /dev/null 2>&1; then
-    curl -fsSL https://www.pgadmin.org/static/packages_pgadmin_org.pub | gpg --dearmor -o /etc/apt/trusted.gpg.d/pgadmin.gpg
-    sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list'
-    sudo apt update
-    apt-get -y install --no-install-recommends pgadmin4
-    fi
-
     PACKAGES_ALREADY_INSTALLED="true"
 
 
