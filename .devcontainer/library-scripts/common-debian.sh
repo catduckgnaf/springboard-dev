c5e51b937bedf153ddcd2730d50022a8c95af1ac
catduckgnaf <github@catduck.org>
2023-12-14 10:33:18 -0500

    switched postgres to own container

--- .devcontainer/library-scripts/common-debian.sh
+++ .devcontainer/library-scripts/common-debian.sh
@@ -154,21 +154,6 @@ if [ "${PACKAGES_ALREADY_INSTALLED}" != "true" ]; then
 
    # Install postgresql (and dependendencies to including pgadmin4) if not already installed (may be more recent than distro version)
 
-    if ! type postgresql-14 > /dev/null 2>&1; then
-    sh -c 'echo "deb https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-    apt-get -y install --no-install-recommends postgresql-14
-
-    fi
-
-    if ! type postgresql-client-14 > /dev/null 2>&1; then
-    apt-get -y install --no-install-recommends postgresql-client-14
-    fi
-
-    if ! type postgresql-plpython3-14 > /dev/null 2>&1; then
-    apt-get -y install --no-install-recommends postgresql-plpython3-14
-    fi
-
     PACKAGES_ALREADY_INSTALLED="true"
 
 
