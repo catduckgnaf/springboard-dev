1dd1d098242ea2c9aa55805bc27e9eb1e476a8ff
CatDuck <129601174+catduckgnaf@users.noreply.github.com>
2023-12-18 12:29:03 -0500

    Update run.sh

--- .devcontainer/library-scripts/run.sh
+++ .devcontainer/library-scripts/run.sh
@@ -1,5 +1,7 @@
 echo 'Installing pip requirements; it will not take long.'
 pip install -r .devcontainer/requirements.txt
+sudo chown $USER /var/lib/pgadmin
+sudo chown $USER /var/log/pgadmin
 clear
 uname -r
 flask --version
