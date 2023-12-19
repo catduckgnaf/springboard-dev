746280b5a65c1357f239d5de6ca8a962aef3794d
catduckgnaf <github@catduck.org>
2023-12-19 01:01:49 -0500

    Update run.sh

--- .devcontainer/library-scripts/run.sh
+++ .devcontainer/library-scripts/run.sh
@@ -1,4 +1,6 @@
 echo 'Installing Springboard SEC pip requirements; it will not take long.'
+mkdir /var/lib/pgadmin
+mkdir /var/log/pgadmin
 pip install -r .devcontainer/requirements.txt
 clear
 uname -r
