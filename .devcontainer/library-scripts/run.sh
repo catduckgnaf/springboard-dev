3dbfa58610eca629cdfdbf0fb33dececccec1b81
CatDuck <129601174+catduckgnaf@users.noreply.github.com>
2023-12-19 14:07:43 -0500

    Update run.sh

--- .devcontainer/library-scripts/run.sh
+++ .devcontainer/library-scripts/run.sh
@@ -1,16 +1,21 @@
+#!/bin/bash
+
 echo 'install final requirements'
 pip install -r .devcontainer/requirements.txt
+
 echo 'installing pgadmin4, last step'
-sudo mkdir /var/lib/pgadmin
-sudo mkdir /var/log/pgadmin
-sudo mkdir /etc/pgadmin
+sudo mkdir -p /var/lib/pgadmin /var/log/pgadmin /etc/pgadmin
 cp .devcontainer/pgadmin4.py /etc/pgadmin/config_system.py
-sudo chown $USER /var/lib/pgadmin
-sudo chown $USER /var/log/pgadmin
+sudo chown $USER:$USER /var/lib/pgadmin /var/log/pgadmin
+
+# Install pgadmin4 using pipx
 pipx install pgadmin4
+
 clear
 uname -r
 flask --version
+
+# Copy and display first-run notice
 cp .devcontainer/first-run-notice-temp.txt .devcontainer/first-run-notice.txt
 cat .devcontainer/first-run-notice.txt
 rm .devcontainer/first-run-notice.txt
