6a579495489934da61563495fbdb4248f40f6d2d
CatDuck <129601174+catduckgnaf@users.noreply.github.com>
2023-12-19 10:07:40 -0500

    Update run.sh

--- .devcontainer/library-scripts/run.sh
+++ .devcontainer/library-scripts/run.sh
@@ -1,7 +1,14 @@
 echo 'Installing Springboard SEC pip requirements; it will not take long.'
-mkdir /var/lib/pgadmin
-mkdir /var/log/pgadmin
 pip install -r .devcontainer/requirements.txt
+clear
+echo 'installing pgadmin4, this is the last step'
+pipx install pgadmin4
+sudo mkdir /var/lib/pgadmin
+sudo mkdir /var/log/pgadmin
+sudo chown $USER /var/lib/pgadmin
+sudo chown $USER /var/log/pgadmin
+echo 'make a username and email for pgadmin4'
+pgadmin4
 clear
 uname -r
 flask --version
